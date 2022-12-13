a762607f934d7f476f69994d0d3cf0f46308f2f8
Ivan Buryak <buryak.ivan@yandex.ru>
2022-12-13 09:54:30 +0500

    Setup project

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Showcase from './Showcase'
+import './Showcase.css'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Showcase />
+  </React.StrictMode>,
+)
